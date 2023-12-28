9e4e7f65cd08bb3a06c2766d684495613d53a230
Enow Divine <sirdivine16@gmail.com>
2023-12-28 10:04:31 +0100

    db connection, and middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 import express, { Express, Request, Response, Application } from "express";
+import bodyParser = require("body-parser");
+import dbConnect from "./config/db";
 import dotenv from "dotenv";
+import cors from "cors";
 
-//For env File
 dotenv.config();
-
+dbConnect();
 const app: Application = express();
-const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
+  res.send("Prepmeets Server");
 });
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
