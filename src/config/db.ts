fec4e3af39fbcf70711d766f0c8c88f13b43a8c6
enowdivine <sirdivine16@gmail.com>
2024-03-10 13:05:01 +0100

    Updates

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,15 +2,20 @@ import "dotenv/config";
 import * as pg from "pg";
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE as string,
-  process.env.POSTGRES_USERNAME as string,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
-    dialectModule: pg,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.POSTGRES_DATABASE as string,
+//   process.env.POSTGRES_USERNAME as string,
+//   process.env.POSTGRES_PASSWORD,
+//   {
+//     host: process.env.POSTGRES_HOST,
+//     dialect: "postgres",
+//     dialectModule: pg,
+//   }
+// );
+
+const sequelize = new Sequelize({
+  dialect: "postgres",
+  storage: process.env.DATABASE_URL,
+});
 
 export default sequelize;
