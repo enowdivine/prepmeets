2cd272bf49709679c2cf1cc83501505f1afa4e68
enowdivine <sirdivine16@gmail.com>
2024-04-03 12:59:09 +0100

    intro video

--- src/config/config.js
+++ src/config/config.js
@@ -2,78 +2,78 @@ const dotenv = require("dotenv")
 const pg = require("pg")
 dotenv.config();
 
-// const url = require('url').parse(process.env.DATABASE_URL);
-// module.exports = {
-//   development: {
-//     database: url.pathname.split('/')[1],
-//     username: url.auth.split(':')[0],
-//     password: url.auth.split(':')[1],
-//     host: url.hostname,
-//     port: url.port,
-//     dialect: 'postgres',
-//     dialectModule: pg,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     }
-//   },
-//   test: {
-//     database: url.pathname.split('/')[1],
-//     username: url.auth.split(':')[0],
-//     password: url.auth.split(':')[1],
-//     host: url.hostname,
-//     port: url.port,
-//     dialect: 'postgres',
-//     dialectModule: pg,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     }
-//   },
-//   production: {
-//     database: url.pathname.split('/')[1],
-//     username: url.auth.split(':')[0],
-//     password: url.auth.split(':')[1],
-//     host: url.hostname,
-//     port: url.port,
-//     dialect: 'postgres',
-//     dialectModule: pg,
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     }
-//   },
-// };
-
+const url = require('url').parse(process.env.DATABASE_URL);
 module.exports = {
   development: {
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
+    database: url.pathname.split('/')[1],
+    username: url.auth.split(':')[0],
+    password: url.auth.split(':')[1],
+    host: url.hostname,
+    port: url.port,
+    dialect: 'postgres',
     dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
+    database: url.pathname.split('/')[1],
+    username: url.auth.split(':')[0],
+    password: url.auth.split(':')[1],
+    host: url.hostname,
+    port: url.port,
+    dialect: 'postgres',
     dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   production: {
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
+    database: url.pathname.split('/')[1],
+    username: url.auth.split(':')[0],
+    password: url.auth.split(':')[1],
+    host: url.hostname,
+    port: url.port,
+    dialect: 'postgres',
     dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 };
+
+// module.exports = {
+//   development: {
+//     username: process.env.POSTGRES_USERNAME,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DATABASE,
+//     host: process.env.POSTGRES_HOST,
+//     dialect: "postgres",
+//     dialectModule: pg,
+//   },
+//   test: {
+//     username: process.env.POSTGRES_USERNAME,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DATABASE,
+//     host: process.env.POSTGRES_HOST,
+//     dialect: "postgres",
+//     dialectModule: pg,
+//   },
+//   production: {
+//     username: process.env.POSTGRES_USERNAME,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DATABASE,
+//     host: process.env.POSTGRES_HOST,
+//     dialect: "postgres",
+//     dialectModule: pg,
+//   },
+// };
--- src/config/db.ts
+++ src/config/db.ts
@@ -2,26 +2,26 @@ import "dotenv/config";
 import * as pg from "pg";
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE as string,
-  process.env.POSTGRES_USERNAME as string,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
-    dialectModule: pg,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.POSTGRES_DATABASE as string,
+//   process.env.POSTGRES_USERNAME as string,
+//   process.env.POSTGRES_PASSWORD,
+//   {
+//     host: process.env.POSTGRES_HOST,
+//     dialect: "postgres",
+//     dialectModule: pg,
+//   }
+// );
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
-//   dialect: "postgres",
-//   protocol: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false, // <<<<<< This line is required for Heroku PostgreSQL
-//     },
-//   },
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // <<<<<< This line is required for Heroku PostgreSQL
+    },
+  },
+});
 
 export default sequelize;
