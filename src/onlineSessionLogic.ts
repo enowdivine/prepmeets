6af2d5ea178ad1bb5a93bc5f5913c588b0eacdf2
enowdivine <sirdivine16@gmail.com>
2024-04-07 10:22:30 +0100

    video conferencing socket connection

--- src/onlineSessionLogic.ts
+++ src/onlineSessionLogic.ts
@@ -1,22 +1,33 @@
 const onlineSessionLogic = (io: any) => {
   // online session logic
+  // io.on("connection", (socket: any) => {
+  //   socket.emit("me", socket.id);
+
+  //   socket.on("disconnect", (socket: any) => {
+  //     socket.broadcast.emit("callEnded");
+  //   });
+
+  //   socket.on("callUser", (data: any) => {
+  //     io.to(data.userToCall).emit("callUser", {
+  //       signal: data.signal,
+  //       from: data.from,
+  //       name: data.name,
+  //     });
+  //   });
+
+  //   socket.on("answerCall", (data: any) => {
+  //     io.to(data.to).emit("callAccepted"), data.signal;
+  //   });
+  // });
+
   io.on("connection", (socket: any) => {
-    socket.emit("me", socket.id);
+    socket.on("join-room", (roomId: string, userId: string) => {
+      socket.join(roomId);
+      socket.to(roomId).broadcast.emit("user-connected", userId);
 
-    socket.on("disconnect", (socket: any) => {
-      socket.broadcast.emit("callEnded");
-    });
-
-    socket.on("callUser", (data: any) => {
-      io.to(data.userToCall).emit("callUser", {
-        signal: data.signal,
-        from: data.from,
-        name: data.name,
+      socket.on("disconnect", () => {
+        socket.to(roomId).broadcast.emit("user-disconnected", userId);
       });
-    });
-
-    socket.on("answerCall", (data: any) => {
-      io.to(data.to).emit("callAccepted"), data.signal;
     });
   });
 };
